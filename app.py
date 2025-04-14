3d8360f53cf4833dd5ec954f3558317818247394
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-04-14 06:46:36 +0100

    Update to use GOOGLE_CREDS_JSON for Render deployment

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
+import json
 
 app = Flask(__name__)
 
@@ -9,7 +11,10 @@ scope = [
     'https://spreadsheets.google.com/feeds',
     'https://www.googleapis.com/auth/drive'
 ]
-creds = creds = ServiceAccountCredentials.from_json_keyfile_name('/etc/secrets/creds.json', scope)
+
+creds_json = os.getenv("GOOGLE_CREDS_JSON")
+creds_dict = json.loads(creds_json)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open("bmi-results").sheet1
 
